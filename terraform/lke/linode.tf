f4370fe10cc207e8d30f31871812271d0acacb44
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-06-05 08:24:46 +0200

    fix: path for kubeconfig
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- terraform/lke/linode.tf
+++ terraform/lke/linode.tf
@@ -22,10 +22,11 @@ resource "null_resource" "kubeconfig" {
 
   triggers = {
     kubeconfig = md5(linode_lke_cluster.lke.kubeconfig)
+    output     = md5(var.kubeconfig)
     script     = filemd5("./scripts/kubeconfig.sh")
   }
 
   provisioner "local-exec" {
-    command = "./scripts/kubeconfig.sh '${var.kubeconfig}' '${linode_lke_cluster.lke.id}' '${linode_lke_cluster.lke.kubeconfig}'"
+    command = "./scripts/kubeconfig.sh '${pathexpand(var.kubeconfig)}' '${linode_lke_cluster.lke.id}' '${linode_lke_cluster.lke.kubeconfig}'"
   }
 }
